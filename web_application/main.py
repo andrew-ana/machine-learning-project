3f4bd9254baf0f36b2d281fdf193110ca9025737
Andrew Anastasiades <atanasta.aa@gmail.com>
2021-08-14 09:42:29 -0400

    Basic Page Setup

--- web_application/main.py
+++ web_application/main.py
@@ -18,3 +18,15 @@ def home():
     data = dict()
     data["page_name"] = "Home"
     return render_template('home.html', data=data)
+
+@main.route('/about')
+def about():
+    data = dict()
+    data["page_name"] = "About"
+    return render_template('about.html', data=data)
+
+@main.route('/other')
+def other():
+    data = dict()
+    data["page_name"] = "Other"
+    return render_template('other.html', data=data)
