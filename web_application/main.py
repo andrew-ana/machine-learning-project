67461721547611b284eeb159361ebac2c836da34
Andrew Anastasiades <atanasta.aa@gmail.com>
2021-08-24 21:51:28 -0400

    Added (broken) form

--- web_application/main.py
+++ web_application/main.py
@@ -2,8 +2,10 @@
 #  We can change the routes and the functionality here
 
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from .load_model import run_prediction
+import requests
 #from . import db
-#from .models import *
+from .models import *
 import json
 import os
 
@@ -58,3 +60,75 @@ def visuals(visual):
         return redirect(url_for('main.visuals', visual="dow"))
     data["display"] = visual
     return render_template('visuals.html', data=data)
+
+@main.route('/PredictAFlight')
+def predict_a_flight():
+    data = dict()
+    data["page_name"] = "Flight Prediction"
+    return render_template('flightprediction.html', data=data)
+
+@main.route('/predict', methods=['POST'])
+def predict():
+    data = dict()
+    flight = dict()
+
+    FLIGHT_API_KEY = "c9a2daca0495eef5267ab156f9195d41"
+    FLIGHT_URL = "http://api.aviationstack.com/v1/flights"
+
+    flight_num = request.form['flight_num']
+
+    data['flight_num'] = flight_num
+    
+    #Aviation API
+    if flight_num:
+        api_response = requests.get(FLIGHT_URL, params={"access_key" : FLIGHT_API_KEY, "flight_iata" : flight_num})
+        if api_response.status_code == 200:
+            api_data = api_response.json()['data']
+            data['api_results'] = api_data
+            flight['Dep_Airport'] = api_data[0]['departure']['iata']
+            flight['Arr_Airport'] = api_data[0]['arrival']['iata']
+            flight['Mkt_Ccode'] = api_data[0]['airline']['iata']
+            scheduled_takeoff = dt.strptime(api_data[0]['departure']['scheduled'], "%Y-%m-%dT%H:%M:%S+00:00")
+            hour = scheduled_takeoff.hour
+            if 0 <= hour < 3:
+                dep_block = "late_night"
+            elif 3 <= hour < 6:
+                dep_block = "early_morning"
+            elif 6 <= hour < 9:
+                dep_block = "morning"
+            elif 9 <= hour < 12:
+                dep_block = "late_morning"
+            elif 12 <= hour < 15:
+                dep_block = "early_afternoon"
+            elif 15 <= hour < 18:
+                dep_block = "late_afternoon"
+            elif 18 <= hour < 21:
+                dep_block = "evening"
+            else:
+                dep_block = "late_evening"
+
+            flight['Sched_Dep_Time_OAG_Block'] = dep_block
+            flight['month'] = scheduled_takeoff.strftime("%b").lower()
+            flight['Day_Week'] = scheduled_takeoff.strftime("%a").lower()
+            run_prediction(flight)
+        else:
+            data['api_results'] = api_response.status_code
+            print(api_response)
+    # DB Query - Get historical flights between endpoints
+    qresults = Airline.query.filter(Airline.Dep_Airport==flight["Dep_Airport"], Airline.Arr_Airport==flight["Arr_Airport"]).limit(100).all()
+    search_results = []
+    if qresults: 
+        for result in qresults:
+            search_results.append([result.Date, result.Arr_Delay_Time_Actual])
+    data['search_results'] = search_results
+    return jsonify(data)
+
+
+#Airline.query.with_entities(Airline.Date, func.count(Airline.Date)).group_by(Airline.Date).all()
+@main.route('/flightsbydate', methods=['POST'])
+def flights_by_date():
+    data = dict()
+    # DB Query
+    qresults = Airline.query.with_entities(Airline.Date, func.count(Airline.Date)).group_by(Airline.Date).all()
+    data['qresults'] = qresults
+    return data
---
+++ web_application/static/js/predict.js
@@ -0,0 +1,68 @@
+// Display paragraphs of 
+$.fn.searchResults = function(searchArray) {
+    this.children().remove();
+    container = this;
+    container.append('<h1 class="text-center">Historical Flights</h1>')
+    searchArray.forEach(flight => {
+        container.append('<p class="text-center"> Departure Date: '+flight[0] + '.   Delay: '+flight[1]+'</p>')
+    });
+}
+
+$.fn.flightList = function(searchArray) {
+    this.children().remove();
+    container = this;
+    searchArray.forEach(date => {
+        container.append('<p class="text-center"> Flight Date:'+date[0]+'.    Number of Flights'+date[1]+'</p>')
+    });
+}
+
+// Predict with form data
+$.fn.prediction = function() {
+    console.log($('#departs-from').val());
+    $.ajax({
+        url: '/predict',
+        data: {
+            flight_num : $('#flight_num').val()
+        },
+        type: 'POST',
+        success: function(response) {
+            $('#loading-icon').hide()
+            $('#form-output').searchResults(response.search_results);
+        },
+        error: function(response){
+            console.log('Bad Response')
+        }
+    })
+};
+
+// Get Number of flights by date
+$.fn.flightsByDate = function() {
+    $.ajax({
+        url: '/flightsbydate',
+        data: {},
+        type: 'POST',
+        success: function(response) {
+            $('#loading-icon').hide()
+            $('#form-output').flightList(response.qresults);
+        },
+        error: function(response){
+            console.log('Bad Response')
+        }
+    })
+};
+
+
+// LISTENER: When the button is clicked, follow route
+$('#form-update').click(function(){
+    $('#form-output').children().remove();
+    $('#loading-icon').show()
+    $(document).prediction();
+});
+
+// LISTENER: When the button is clicked, follow route
+$('#flight-summary').click(function(){
+    $('#form-output').children().remove();
+    $('#loading-icon').show()
+    $(document).flightsByDate();
+});
+
